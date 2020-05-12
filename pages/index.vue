8a25234caaf3552dc544b9d5a0da0f14a367edaa
himeoka <himeoka@stoopa.org>
2020-05-12 18:45:39 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template lang="pug">
 div.p_index.contents.padding(ref="contents")
   div.contents_inner.p_index_inner.inner.padding(ref="contents_inner",:class="{fontready:$store.state.fontready,imageready:$store.state.imageready}")
-
+    div.p_index_top
+      div.p_index_top_list
+        article.p_index_top_list_block(v-for="item in items")
+          nuxt-link(:to="'/news/' + item.id")
+            p.p_index_top_list_block_image
+              img.lazyload(:src="dummyImg",:data-src="item.image.url", :data-srcset="item.image.url+' 1x,\' + item.image.url + '?dpr=2 2x'", alt="")
+            h1.p_index_top_list_block_ttl {{item.title}}
+          div.p_index_top_list_block_tag
+            div.p_news_article_tag_block(v-for="item in item.ctg")
+              nuxt-link(:to="'/ctg/' + item.id") {{item.name}}
+              
 
 </template>
 <script>
@@ -29,6 +39,18 @@ export default {
       page:"index",
       des:"",
       items:[]
+    }
+  },
+  async asyncData({app,store}) {
+    console.log()
+    const { data } = await app.$axios.get(
+      store.state.api + "/news?fields=title,image,ctg,id",
+      {
+        headers: { "X-API-KEY":"e8e956a4-4a09-4c71-ac0c-60b577553a3b" }
+      }
+    );
+    return {
+      items: data.contents
     }
   },
   updated(){
